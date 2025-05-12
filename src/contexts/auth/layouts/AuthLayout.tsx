795b26950618d1a920064f1a0619951b8d4a5892
Carles Vargas <carlesvargas5@gmail.com>
2025-05-12 12:09:23 +0200

    refactor(auth): simplify AuthLayout structure and update styling

--- src/contexts/auth/layouts/AuthLayout.tsx
+++ src/contexts/auth/layouts/AuthLayout.tsx
@@ -5,34 +5,19 @@ export default function AuthLayout() {
   const t = useT();
 
   return (
-    <div className="flex min-h-screen bg-[url('/assets/images/backgrounds/bg-auth.avif')] bg-cover bg-center">
-      <div className="hidden lg:flex bg-black/10 lg:w-1/2 relative items-center justify-center">
-        <div className="relative z-10 text-center p-8">
-          <h1 className="text-4xl font-bold text-white mb-2 text-shadow-lg">
-            {t('authLayout.h1')}
-          </h1>
-          <p className="text-2xl text-white/85 uppercase tracking-wider text-shadow-lg">
-            {t('authLayout.p')}
-          </p>
+    <section className="bg-[url('/assets/images/backgrounds/abstract-black-white.avif')] bg-cover bg-center">
+      <div className="flex min-h-screen flex-col justify-center bg-gray-50/95 py-12 sm:px-6 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-md">
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">ColabOS</h2>
+          <p className="mt-2 text-center text-sm text-gray-600">{t('authLayout.descr')}</p>
         </div>
-      </div>
 
-      <div className="w-full lg:w-1/2 bg-black/60 flex items-center justify-center p-8">
-        <div className="w-full max-w-md space-y-8">
-          <div className="text-center">
-            <h2 className="text-3xl font-bold text-white">{t('authLayout.h2')}</h2>
-            <p className="mt-2 text-gray-400">{t('authLayout.descr')}</p>
-          </div>
-
-          <div className="mt-8 flex flex-col items-center">
+        <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+          <div className="flex flex-col items-center sm:rounded-lg sm:px-10">
             <Outlet />
           </div>
-
-          <div className="mt-8 text-center text-sm text-gray-500">
-            <p>© 2024 ColabOS. {t('authLayout.footer')}</p>
-          </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
